582e5fc8c82edcab14d3754fbf0ca2da00840b49
Khoa Luong <kluon7@ucsc.edu>
2018-02-15 00:04:28 -0800

    added account-password functionality

--- artdump/imports/ui/HomePage.jsx
+++ artdump/imports/ui/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 export class HomePage extends React.Component {
   constructor() {
@@ -10,11 +10,14 @@ export class HomePage extends React.Component {
   }
 
   render() {
+    //console.log(Meteor.userId())
     return <div>
         <h1> This is the Home Page. </h1>
-        <Link to="/login"> To Login page </Link>
-        <Link to="/registration"> To registration page </Link>
-        <Link to="/"> To main page </Link>
+        <div><Link to="/login"> To Login page </Link></div>
+        <div><Link to="/registration"> To Registration page </Link></div>
+        <div><Link to="/createpost"> To Create Post page</Link></div>
+        <div><Link to="/"> To main page </Link></div>
+        <div><AccountsUIWrapper/></div>
     </div>
   }
 }
