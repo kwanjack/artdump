91fb837b19803327cef32b43138fe76c816cf801
kwanjack <jack.ks.kwan@gmail.com>
2018-02-20 21:44:53 -0800

    Add withTracker usage on HomePage

--- artdump/imports/ui/HomePage.jsx
+++ artdump/imports/ui/HomePage.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export class HomePage extends React.Component {
+class HomePage extends React.Component {
   constructor() {
     super();
     this.state = {}
   }
 
+
   render() {
-    //console.log(Meteor.userId())
     return <div>
         <h1> This is the Home Page. <i className="fa fa-home"> </i> </h1>
         <div><Link to="/login"> To Login page </Link></div>
         <div><Link to="/registration"> To Registration page </Link></div>
-        <div><Link to="/createpost"> To Create Post page</Link></div>
+        {(this.props.currentUser != null &&
+          <div><Link to="/createpost"> To Create Post page</Link></div>
+        )}
         <div><Link to="/"> To main page </Link></div>
         <div><AccountsUIWrapper/></div>
     </div>
   }
 }
+
+export default withTracker(() => {
+  return {
+    currentUser: Meteor.user()
+  };
+})(HomePage);
