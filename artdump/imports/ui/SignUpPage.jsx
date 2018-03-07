2e10ab2415f1da1f4f7ad6b8765b7ecc7c3314e7
kwanjack <jack.ks.kwan@gmail.com>
2018-03-06 23:46:45 -0800

    Dont use Accounts-UI anymore, use our own separate pages

---
+++ artdump/imports/ui/SignUpPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Link } from 'react-router-dom';
+import { Posts } from '../api/post.jsx';
+import { Redirect, Switch } from 'react-router-dom'
+import { withTracker } from 'meteor/react-meteor-data';
+import HomePage from './HomePage';
+import Navbar from './Navbar.jsx'
+
+class SignUpPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {redirectToHome: false}
+  }
+
+	createAccount(event) {
+    event.preventDefault();
+    let { username, password } = this.state;
+    let user = { username, password };
+
+    Accounts.createUser(user, err => {
+      if (err) {
+        console.log('err:', err);
+        this.setState({ error: err.reason });
+      } else {
+        this.props.history.push('/');
+      }
+    });
+	}
+
+  render() {    
+    return <div>
+        <div>
+          <Navbar
+            path={this.props.match.path}/>
+        </div>
+        <h1> Sign Up </h1>
+        <form onSubmit={this.createAccount.bind(this)}>
+          <div>
+            <label><b>Username</b></label>
+            <input onChange={ (e) => this.setState({ username: e.target.value }) } type="text" placeholder="username" required/>
+          </div>
+
+          <div>
+            <label><b>Password</b></label>
+            <input onChange= {(e) => this.setState({ password: e.target.value }) } type="password" placeholder="password" required/>
+          </div>
+
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+
+          { this.state.error ? <div> this.state.error </div> : null }
+          
+        </form>
+
+    </div>
+  }
+}
+
+export default withTracker(() => {
+  let currentUser = Meteor.user() ? Meteor.user() : {};
+  return {
+    currentUser,
+  };
+})(SignUpPage);
